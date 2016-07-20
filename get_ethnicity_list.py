d37bcd8e64081ab1739a4a11933358102cf43ac5
Fatbab <Fatbab@users.noreply.github.com>
2016-07-20 13:50:16 -0400

    Update get_ethnicity_list.py

--- get_ethnicity_list.py
+++ get_ethnicity_list.py
@@ -1,28 +1,18 @@
+## Given a list of lastnames, we get corresponding ethnicity based on values from pickle file: D4name_ethnicity.pkl
+
 import jellyfish
 import pickle
 
-
-pkl_file = open('/Users/fatemeh/Documents/Imperial/SpringBlock1/WorkforceAnalytics/Lecture4/Code/inClass/D4name_ethnicity.pkl','rb')
+# Load pickle file
+pkl_file = open('/PATH/TO/D4name_ethnicity.pkl','rb')
 D4name_ethnicity=pickle.load(pkl_file)
 pkl_file.close()
 
+# Create a dictionary with Ethnicity
 D4name_ethnicity_meta ={}
 for k in D4name_ethnicity.keys():
     D4name_ethnicity_meta[jellyfish.metaphone(unicode(k))]= D4name_ethnicity[k]
 
-
-'''
-def get_ethnicity_list(input_list):
-    output_dict={} #dict((k,'') for k in input_list)
-    for k in input_list:
-        temp_lastname =jellyfish.metaphone(unicode(k))
-        if D4name_ethnicity_meta.has_key(temp_lastname):
-            output_dict[k]= D4name_ethnicity_meta[temp_lastname]
-        else:
-            output_dict[k]= 'other'
-
-    return output_dict
-'''
 
 def get_ethnicity_list(input_list):
     output_list =[]
