c2eabb23bdb06a0edc8f88aaa7ade574a92b4752
Fatbab <Fatbab@users.noreply.github.com>
2016-07-20 13:55:49 -0400

    Update get_ethnicity_list.py

--- get_ethnicity_list.py
+++ get_ethnicity_list.py
@@ -1,19 +1,20 @@
-## Given a list of lastnames, we get corresponding ethnicity based on values from pickle file: D4name_ethnicity.pkl
+## Given a list of lastnames, we get corresponding ethnicities based on values from pickle file: D4name_ethnicity.pkl
 
 import jellyfish
 import pickle
 
-# Load pickle file
+## Load pickle file
 pkl_file = open('/PATH/TO/D4name_ethnicity.pkl','rb')
 D4name_ethnicity=pickle.load(pkl_file)
 pkl_file.close()
 
-# Create a dictionary with Ethnicity
-D4name_ethnicity_meta ={}
-for k in D4name_ethnicity.keys():
-    D4name_ethnicity_meta[jellyfish.metaphone(unicode(k))]= D4name_ethnicity[k]
+## Create a dictionary with key: metaphonic version of lastnames from D4name_ethnicity and value: ethnicity.
+D4name_ethnicity_meta ={}    
+for k in D4name_ethnicity.keys():    
+    D4name_ethnicity_meta[jellyfish.metaphone(unicode(k))]= D4name_ethnicity[k]    
 
-
+## Matches metaphonic version of items from input_list to the dictionary we have from previous step.   
+## Returns a list of corresponding ethnicites, where unmatched items are marked 'other'.
 def get_ethnicity_list(input_list):
     output_list =[]
     for i in input_list:
